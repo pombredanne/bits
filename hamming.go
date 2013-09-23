bad622f51a53a40aa25277a424fd2863da7af409
twmb <travis.bischel@gmail.com>
2013-09-22 22:23:27 -0500

    Removed slower functions.
    For the purpose of benchmarking, I copied the following
    Hamming function:
    https://github.com/niven/simhashing/blob/master/util.go#L48
    which is just Kernighan's hamming function implemented:
    http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan
    
    This hamming function is slower than the DistanceBits
    implementation, which is slower than a lookup table:
    BenchmarkDistance         2000      845001 ns/op
    BenchmarkHamDistance       500     3398084 ns/op
    BenchmarkDistanceBits      500     3005490 ns/op
    
    Both DistanceBits and HammingDistance functions are removed with this commit.
    
    All benchmarks are removed and the test (but the function is correct).

--- hamming.go
+++ hamming.go
@@ -21,29 +21,6 @@ var table [256]int = [256]int{
 	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
 	4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8}
 
-func HammingDistance(a uint64, b uint64) int {
-
-	// keep only the bits that are different
-	x := a ^ b
-
-	count := 0
-	for ; x > 0; count++ {
-		x &= x - 1 // clear the least significant bit set
-	}
-
-	return count
-}
-
-func DistanceBits(a, b uint64) int {
-	x := a ^ b
-
-	distance := uint64(0)
-	for ; x > 0; x >>= 1 {
-		distance += x & 1
-	}
-	return int(distance)
-}
-
 // Distance returns the hamming distance between two integers.
 func Distance(a, b int) int {
 	x := uint(a ^ b)
